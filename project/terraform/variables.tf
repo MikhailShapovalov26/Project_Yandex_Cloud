d3813896bb54e5f8a38cc2fa05c253bd3a0eb14b
Mihail.Shapovalov <mikhail.shapowalow@yandex.ru>
2022-09-16 18:56:03 +0300

    read terraform and DNS Zone YC

--- project/terraform/variables.tf
+++ project/terraform/variables.tf
@@ -2,3 +2,11 @@ variable "subnet_id" {
   description = "Yandex VPC subnet_id"
   type        = string
 }
+variable "subdomai" {
+  type = list(string)
+  description = "Hostnames list"
+}
+variable "dns-domain" {
+  type = string
+  description = "DNS zone"
+}
